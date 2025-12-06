2b67769ded6b9a17e4b7cc84a503b015fc0ecaff
Gustav Alerby <me@gurra.rocks>
2025-12-06 22:49:54 +0100

    day 6

---
+++ 2025/day06/pt1.py
@@ -0,0 +1,27 @@
+import sys
+import math
+import json
+
+input_file = sys.argv[1]
+input = open(input_file, "r")
+lines = input.read().split("\n")
+len_lines = len(lines)
+l = []
+num_stores = len(lines[0].split())
+data = {}
+for s in range(0, num_stores):
+    data[s] = []
+for j in lines:
+    for i, n in enumerate(j.split()):
+        data[i].append(n)
+
+for i, stuff in data.items():
+    op = stuff[-1]
+    del stuff[-1]
+    ints = [int(x) for x in stuff]
+    if op == '+':
+        #print(sum(int_list))
+        l.append(sum(ints))
+    if op == '*':
+        l.append(math.prod(ints))
+print(f"The Cephalopod sum is: {sum(l)}")
